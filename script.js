1b436e6f4e90e32a469cb8c70ef693af3898d2d8
sungketpatel <93491754+Sungket@users.noreply.github.com>
2023-07-05 16:37:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello, welcome to the Dodgyclunk Mechanic Diagnostics App! How can I help you today?");
+
+const question = response => {
+    const keyWords = ["help", 'problem', 'not working', 'faulty'];
+    if (response.)
+}
