7365da06e17df2e7f6b2a43906bcb9fbb84dbf73
sungketpatel <93491754+Sungket@users.noreply.github.com>
2023-07-05 17:40:23 +0100

    change of idea to exercise selector

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-console.log("Hello, welcome to the Dodgyclunk Mechanic Diagnostics App! How can I help you today?");
+const exercises = ["help", 'problem', 'not working', 'faulty'];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const equipment = ['Pull up bar', 'dip bars', 'bench', 'box', 'step', 'barbell', 'dumbells'];
 
-const question = response => {
-    const keyWords = ["help", 'problem', 'not working', 'faulty'];
-    if (response.)
-}
+let randomIndex = Math.floor(Math.random() * 10);
