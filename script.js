ff45861df42f7c591eb298a97890afbf221ad5e8
Sungket Patel <93491754+Sungket@users.noreply.github.com>
2023-07-06 12:35:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const exercises = ["Pull-ups", 'dips', 'push-ups', 'squats', 'pistol-squats', 'handstand push-ups', 'pike push-ups', 'bench-press', 'lateral raises', 'dumbell press'];
+const exercises = ['Pull-ups', 'dips', 'push-ups', 'squats', 'pistol-squats', 'handstand push-ups', 'pike push-ups', 'bench-press', 'lateral raises', 'dumbell press'];
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const equipment = ['Pull-up bar', 'dip bars', 'bench', 'box', 'step', 'barbell', 'dumbells'];
+//const equipment = ['Pull-up bar', 'dip bars', 'bench', 'box', 'step', 'barbell', 'dumbells'];
+let equipment = [];
 
 let randomEquipmentIndex = Math.floor(Math.random() * 7);
 
@@ -22,40 +23,25 @@ while ((exercise5 === exercise1) || (exercise5 === exercise2) || (exercise5 === 
     exercise5 = exercises[Math.floor(Math.random() * exercises.length)]
 }
 
-let equipment1 = equipment[Math.floor(Math.random() * equipment.length)];
-if(exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'Pull-ups'){
-    equipment1 = equipment[0];
-} else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'dips'){
-    equipment1 = equipment[1];
-} else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'bench-press'){
-    equipment1 = equipment[5];
-} else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'dumbell press'){
-    equipment1 = equipment[6];
-}
-let equipment2 = equipment[Math.floor(Math.random() * equipment.length)];
-while(equipment2 === equipment1){
-    if(exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'Pull-ups'){
-        equipment2 = equipment[0];
-    } else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'dips'){
-        equipment2 = equipment[1];
-    } else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'bench-press'){
-        equipment2 = equipment[5];
-    } else if (exercise1 || exercise2 || exercise3 || exercise4 || exercise5 === 'dumbell press'){
-        equipment2 = equipment[6];
-    } else {
-        equipment2 = equipment[Math.floor(Math.random() * equipment.length)];  
-    }  
-}
-let equipment3 = equipment[Math.floor(Math.random() * equipment.length)];
-while(equipment3 === equipment1 || equipment3 === equipment2){
-    equipment3 = equipment[Math.floor(Math.random() * equipment.length)];
+if (exercise1 === 'Pull-ups'|| exercise2 ==='Pull-ups'|| exercise3 ==='Pull-ups'|| exercise4==='Pull-ups' || exercise5 === 'Pull-ups'){
+    equipment.push('Pull-up bar');
+} 
+if (exercise1==='dips' || exercise2==='dips' || exercise3==='dips' || exercise4==='dips' || exercise5==='dips' === 'dips'){
+    equipment.push('dip-bars');
+} 
+if (exercise1==='bench-press' || exercise2==='bench-press' || exercise3 ==='bench-press'|| exercise4==='bench-press' || exercise5 === 'bench-press'){
+    equipment.push('bench');
+    equipment.push('barbell');
+} 
+if (exercise1==='dumbell press' || exercise2==='dumbell press' || exercise3==='dumbell press' || exercise4 ==='dumbell press'|| exercise5 === 'dumbell press'){
+    equipment.push('dumbells');
 }
 
 
 const generateWOD = () => {
     const day = days[Math.floor(Math.random() * 7)]; 
     console.log(`Today is ${day}, and your workout of the day is: ${exercise1}, ${exercise2}, ${exercise3}, ${exercise4} and ${exercise5}. \n
-The equipment you will need is: ${equipment1}, ${equipment2} and ${equipment3}.`);
+The equipment you will need is: ` + equipment);
 }
 
 generateWOD();
